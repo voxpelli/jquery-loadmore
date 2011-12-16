90fc7a82b5b1bbd02eeb6dee9999d6b2d680100e
Pelle Wessman <pelle@kodfabrik.se>
2011-12-16 11:28:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+loadMore.js
+=======
+
+loadMore.js is a jQuery plugin for easily adding a "more"-link based pagination to an element on ones site.
+
+## Usage
+
+Simple:
+
+    $('.list').loadmore('/path/to/more/html');
+
+Advanced:
+
+    $('.list').loadmore('/path/to/more/html', {
+      'className' : '',
+      text : 'More',
+      loadingText : 'Loading',
+      page : 0,
+      pageSize : false,
+      'pageParam' : 'page',
+      loaded : false
+    });
+
+## Advanced options
+
+* **className** - extra classes to add to the more-link container
+* **text** - text to use in the more-link - should be either a string or a function
+* **loadingText** - text to use in the more-link when it is loading - should be either a string or a function
+* **page** - which page should be fetched on the first more-click?
+* **pageSize** - how many elements should be expected on a new page? If less than this amount is received we've reached the end and removes the more-link.
+* **pageParam** - what's the name of the query parameter for the page to fetch in the AJAX-requests?
+* **loaded** - a function to execute once a new page has been loaded
---
+++ loadmore.js
@@ -0,0 +1,84 @@
+/*jslint browser: true, maxerr: 50, indent: 2 */
+/*global jQuery: false */
+(function ($) {
+  "use strict";
+
+  // Below taken from tipsy.js
+  var maybeCall = function (value, context) {
+    return (typeof value === 'function') ? (value.call(context)) : value;
+  };
+
+  $.fn.loadMore = function (url, options) {
+    var done, update;
+
+    options = $.extend({}, $.fn.loadMore.defaults, options);
+
+    done = function () {
+      var $this = $(this);
+      if (options.done) {
+        options.done.call($this);
+      }
+      $this.remove();
+    };
+
+    update = function () {
+      var $this = $(this), $text = $this.children('span.text'), requestedPage = options.page;
+
+      if ($this.hasClass('loading')) {
+        return false;
+      }
+
+      $this.addClass('loading');
+      $text.text(maybeCall(options.loadingText, $text[0]));
+
+      $.get(url, {page : (options.page + 1)})
+        .fail(function () {
+          //TODO: Do the fail dance
+        })
+        .done(function (data) {
+          if (requestedPage !== options.page) {
+            return;
+          }
+
+          options.page += 1;
+          $this.removeClass('loading');
+          $text.text(maybeCall(options.text, $text[0]));
+
+          var $newData = $(data).filter('*').insertBefore($this);
+
+          if (options.pageSize !== false && $newData.length < options.pageSize) {
+            done.call($this[0]);
+          }
+
+          if (options.loaded && options.loaded.call($newData) === false && $this.closest('.more').length) {
+            done.call($this[0]);
+          }
+        });
+      return false;
+    };
+
+    this.each(function () {
+      var $more = $('<a />', {
+          'class' : 'more ' + options.className,
+          'href' : '#'
+        }),
+        $text = $('<span />', {'class' : 'text'});
+      $text
+        .appendTo($more)
+        .text(maybeCall(options.text, $text[0]));
+      $more.appendTo(this).click(update);
+    });
+
+    return this;
+  };
+
+  $.fn.loadMore.defaults = {
+    'className' : '',
+    text : 'More',
+    loadingText : 'Loading',
+    page : 0,
+    pageSize : false,
+    'pageParam' : 'page',
+    loaded : false
+  };
+}(jQuery));
